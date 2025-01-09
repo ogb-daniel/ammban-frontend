d60ac5c7aa6cfd0d0436dabd96e3b331d797022e
ogb-daniel <ogbuikedaniel@gmail.com>
2025-01-09 18:29:44 +0100

    font setup

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,2 +1,5 @@
-import { Inter } from "next/font/google";
-export const inter = Inter({ subsets: ["latin"] });
+import { Montserrat } from "next/font/google";
+export const montserrat = Montserrat({
+  weight: ["400", "500", "600", "700"],
+  subsets: ["latin"],
+});
