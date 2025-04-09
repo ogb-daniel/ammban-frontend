a515b0a26d1739fb82236747d6f54317b7fe7c03
ogb-daniel <ogbuikedaniel@gmail.com>
2025-04-09 19:06:20 +0100

    refactor: remove unused request parameter from GET functions in user routes

--- app/api/user/getAllStates/route.ts
+++ app/api/user/getAllStates/route.ts
@@ -1,7 +1,7 @@
-import { NextRequest, NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 import { getSession } from "@/app/lib/session";
 const baseUrl = process.env.API_URL;
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const session = await getSession();
     if (!session.accessToken) {
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -26,7 +26,7 @@ export async function POST(request: NextRequest) {
   }
 }
 
-export async function GET(request: NextRequest) {
+export async function GET() {
   try {
     const session = await getSession();
     if (!session.accessToken) {
