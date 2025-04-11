24dc0073c8f780ec28cb37d84d86cef2e93c43ad
ogb-daniel <ogbuikedaniel@gmail.com>
2025-04-11 07:48:16 +0100

    refactor: update deployment workflow for Next.js app and ensure standalone output configuration

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,8 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "standalone",
+  reactStrictMode: true,
+  distDir: "build",
 };
 
 export default nextConfig;
