0591626564e6bf657d2566902efc2c6444e30660
ogb-daniel <ogbuikedaniel@gmail.com>
2025-07-16 09:04:07 +0100

    feat: Add FAQ section to static data; remove unused actions from CommissionTable component

--- app/ui/commission/commision-table.tsx
+++ app/ui/commission/commision-table.tsx
@@ -3,7 +3,6 @@ import React from "react";
 import Table from "../table";
 import { ColumnDef } from "@tanstack/react-table";
 import { useAdminStore } from "@/providers/admin-store-provider";
-import { FaTrash } from "react-icons/fa";
 
 import { LiaUserShieldSolid } from "react-icons/lia";
 import { PiDivideFill } from "react-icons/pi";
@@ -31,32 +30,13 @@ const commissionColumns: ColumnDef<CommissionPercentage>[] = [
 
 const CommissionTable = () => {
   const { commissionsPercentage } = useAdminStore((state) => state);
-  const actions = [
-    // {
-    //   element: <MdEdit className="w-4 h-4 text-[#0B1739]" />,
-    //   onClick: (commission: Commission) => {
-    //     router.push(`${ADMIN_COMMISSION.url}/${commission.id}`);
-    //   },
-    // //   label: "Edit commission",
-    // // },
-    // {
-    //   element: <FaTrash className="w-4 h-4 text-red-500" />,
-    //   onClick: (commission: CommissionPercentage) => {
-    //     // Handle delete action
-    //     if (confirm("Are you sure you want to delete this commission?")) {
-    //       // Add delete logic here
-    //       console.log("Delete commission:", commission);
-    //     }
-    //   },
-    //   label: "Delete commission",
-    // },
-  ];
+  
   return (
     <Table<CommissionPercentage>
       data={commissionsPercentage}
       columns={commissionColumns}
       title="All Commissions"
-      actions={actions}
+      actions={[]}
     />
   );
 };
