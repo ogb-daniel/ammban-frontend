b6501afc60f6cd5f6c54341c1b431f8b64173a9a
Daniel Ogbuike <ogbuikedaniel@gmail.com>
2026-01-12 11:13:47 +0100

    remove change pin

--- app/ui/layouts/desktop-profile-layout.tsx
+++ app/ui/layouts/desktop-profile-layout.tsx
@@ -67,20 +67,26 @@ export default function DesktopProfileLayout({
         <aside className="max-w-64 w-full p-4">
           <ScrollArea className="h-full pr-2">
             <nav className="space-y-2">
-              {links.map((link) => (
-                <Link
-                  key={link.href}
-                  href={link.href}
-                  className={cn(
-                    "block px-4 py-2 rounded-md text-sm font-medium transition-colors",
-                    pathname === link.href
-                      ? "bg-primary text-black bg-[#EBF3FF]"
-                      : "text-muted-foreground hover:bg-[#EBF3FF]"
-                  )}
-                >
-                  {link.title}
-                </Link>
-              ))}
+              {links
+                .filter((link) =>
+                  link.title === "Change Pin" && user?.role === "admin"
+                    ? false
+                    : true
+                )
+                .map((link) => (
+                  <Link
+                    key={link.href}
+                    href={link.href}
+                    className={cn(
+                      "block px-4 py-2 rounded-md text-sm font-medium transition-colors",
+                      pathname === link.href
+                        ? "bg-primary text-black bg-[#EBF3FF]"
+                        : "text-muted-foreground hover:bg-[#EBF3FF]"
+                    )}
+                  >
+                    {link.title}
+                  </Link>
+                ))}
             </nav>
           </ScrollArea>
         </aside>
