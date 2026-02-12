bd3499ba56f0207734b5d2551271f5f8d81372d6
Daniel Ogbuike <ogbuikedaniel@gmail.com>
2026-02-12 09:56:52 +0100

    refresh auth middleware setup

--- middleware.ts
+++ middleware.ts
@@ -7,12 +7,60 @@ const publicRoutes = [
   "/faqs",
   "/verify-account",
 ];
-export function middleware(request: NextRequest) {
-  const token = request.cookies.get("accessToken")?.value;
+export async function middleware(request: NextRequest) {
+  let token = request.cookies.get("accessToken")?.value;
+  const refreshToken = request.cookies.get("refreshToken")?.value;
   const role = request.cookies.get("role")?.value;
 
   const pathname = request.nextUrl.pathname;
   const isPublicRoute = publicRoutes.includes(pathname);
+  console.log("token", token);
+  console.log("refreshToken", refreshToken);
+  console.log("role", role);
+  console.log("pathname", pathname);
+  console.log("isPublicRoute", isPublicRoute);
+
+  if (!token && refreshToken && !isPublicRoute) {
+    try {
+      const refreshResponse = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/api/TokenAuth/RefreshToken`,
+        {
+          method: "POST",
+          body: JSON.stringify({ refreshToken }),
+          headers: {
+            "Content-Type": "application/json",
+          },
+        },
+      );
+      const data = await refreshResponse.json();
+      if (data?.result?.accessToken) {
+        const newAccessToken = data?.result?.accessToken;
+        const newRefreshToken = data?.result?.refreshToken;
+        const newExpiresIn = data?.result?.expireInSeconds;
+        const newRefreshExpiresIn = data?.result?.refreshTokenExpireInSeconds;
+        token = newAccessToken;
+        const response = NextResponse.next();
+        response.cookies.set("accessToken", newAccessToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          maxAge: newExpiresIn,
+          sameSite: "lax",
+          path: "/",
+        });
+
+        response.cookies.set("refreshToken", newRefreshToken, {
+          httpOnly: true,
+          secure: process.env.NODE_ENV === "production",
+          maxAge: newRefreshExpiresIn,
+          sameSite: "lax",
+          path: "/",
+        });
+        return response;
+      }
+    } catch (error) {
+      console.error("Middleware refresh failed:", error);
+    }
+  }
 
   // Allow public routes without token
   if (isPublicRoute && !token) {
